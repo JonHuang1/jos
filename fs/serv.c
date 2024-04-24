4e6c4d15a7cc996326994ce0c97c26e38a6720ac
JonHuang1 <jonhuang918@gmail.com>
2024-04-24 17:06:36 -0400

    85/150

--- fs/serv.c
+++ fs/serv.c
@@ -214,7 +214,21 @@ serve_read(envid_t envid, union Fsipc *ipc)
 		cprintf("serve_read %08x %08x %08x\n", envid, req->req_fileid, req->req_n);
 
 	// Lab 5: Your code here:
-	return 0;
+	struct OpenFile *ofile;
+	int r;
+
+	if ((r = openfile_lookup(envid, req->req_fileid, &ofile)) < 0) {
+		return r;
+	}
+
+	if ((r = file_read(ofile->o_file, ret->ret_buf, req->req_n, ofile->o_fd->fd_offset)) < 0) {
+		return r;
+	}
+	else {
+		ofile->o_fd->fd_offset += r;
+	}
+
+	return r;
 }
 
 
@@ -229,7 +243,22 @@ serve_write(envid_t envid, struct Fsreq_write *req)
 		cprintf("serve_write %08x %08x %08x\n", envid, req->req_fileid, req->req_n);
 
 	// LAB 5: Your code here.
-	panic("serve_write not implemented");
+	// panic("serve_write not implemented");
+	struct OpenFile *ofile;
+	int r;
+
+	if ((r = openfile_lookup(envid, req->req_fileid, &ofile)) < 0) {
+		return r;
+	}
+
+	if ((r = file_write(ofile->o_file, req->req_buf, req->req_n, ofile->o_fd->fd_offset)) < 0) {
+		return r;
+	}
+	else {
+		ofile->o_fd->fd_offset += r;
+	}
+	
+	return r;
 }
 
 // Stat ipc->stat.req_fileid.  Return the file's struct Stat to the
